b0ff7664aabc27637b694df3bcbb9e202d73ed80
Enzo Zambon <eazambon01@gmail.com>
2026-02-04 08:46:55 -0300

    Actualizo carga de datos y EDA

--- src/Cargar_datos.py
+++ src/Cargar_datos.py
@@ -2,12 +2,11 @@ import os
 import pandas as pd
 
 def cargar_datos():
-    "Funcion para Cargar Datos desde un Archivo de Excel"
-
-    script_dir = os.path.dirname(__file__)  # Directorio del script actual
-
-    file_path = os.path.join(script_dir, 'Base_de_datos.xlsx')
-
+    file_path = r"C:\Users\Enzo\OneDrive\Escritorio\Data Science Henry\Modulo 5\MLOps---DSPTM5\Base_de_datos.xlsx"
+    print("Leyendo archivo desde:", file_path)
     df = pd.read_excel(file_path)
-
     return df
+
+
+
+
